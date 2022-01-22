129d19bbd0f32f09773eadfe4883019e0f918d6f
Utkarsh Patel <imutkarshpatel@gmail.com>
2022-01-22 13:20:40 +0530

    added `scale`, `rotate` & `resetChannel` method to `bmp::Image`

--- src/lib/bmp.cpp
+++ src/lib/bmp.cpp
@@ -137,21 +137,146 @@ bmp::Image bmp::Image::toGrayScale() {
 	assert(channel == 3);
 
 	// grayscale bmp::Image object
-	bmp::Image gray;
-	gray.pixels = new unsigned char** [1]; // grayscale bmp has only one channel
-	gray.pixels[0] = new unsigned char* [header.height];
+	bmp::Image image;
+	image.pixels = new unsigned char** [1]; // grayscale bmp has only one channel
+	image.pixels[0] = new unsigned char* [header.height];
 	for (int i = 0; i < header.height; i++) {
-		gray.pixels[0][i] = new unsigned char[header.width];
+		image.pixels[0][i] = new unsigned char[header.width];
 	}
 	for (int i = 0; i < header.height; i++) {
 		for (int j = 0; j < header.width; j++) {
-			gray.pixels[0][i][j] = pixels[0][i][j] * 0.30 + pixels[1][i][j] * 0.59 + pixels[2][i][j] * 0.11;
+			image.pixels[0][i][j] = pixels[0][i][j] * 0.30 + pixels[1][i][j] * 0.59 + pixels[2][i][j] * 0.11;
 		}
 	}
-	gray.header = header;
-	gray.header.bpp = 8;
-	gray.header.offset = 1078;
-	return gray;
+	image.header = header;
+	image.header.bpp = 8;
+	image.header.offset = 1078;
+	return image;
+}
+
+/**
+* Constructs a rotated version of bmp::Image object
+* Input:
+*	degrees:	degrees to rotate (anti-clockwise)
+*/
+bmp::Image bmp::Image::rotate(double degrees) {
+	bmp::Image image;
+	
+	// convert degrees into radians
+	double radians = (degrees * 2 * acos(-1.0)) / 360;
+	double sine = sin(radians);
+	double cosine = cos(radians);
+
+	// transforming the edge points
+	std::pair<double, double> point1 = { header.height * sine * -1, header.height * cosine };
+	std::pair<double, double> point2 = { header.width * cosine - header.height * sine, header.height * cosine + header.width * sine };
+	std::pair<double, double> point3 = { header.width * cosine, header.width * sine };
+
+	double minX = std::min(0.0, std::min(point1.first, std::min(point2.first, point3.first)));
+	double minY = std::min(0.0, std::min(point1.second, std::min(point2.second, point3.second)));
+	double maxX = std::max(point1.first, std::max(point2.first, point3.first));
+	double maxY = std::max(point1.second, std::max(point2.second, point3.second));
+
+	image.header = header;
+	uint32_t newWidth = static_cast<uint32_t>(ceil(fabs(maxX) - minX));
+	uint32_t newHeight = static_cast<uint32_t>(ceil(fabs(maxY) - minY));
+
+	int channel = header.bpp / 8;
+	image.pixels = new unsigned char** [channel];
+	for (int i = 0; i < channel; i++) {
+		image.pixels[i] = new unsigned char* [header.height];
+		for (int j = 0; j < header.height; j++) {
+			image.pixels[i][j] = new unsigned char[header.width];
+			for (int k = 0; k < header.width; k++) {
+				image.pixels[i][j][k] = 0;
+			}
+		}
+	}
+
+	// filling pixels value
+	for (int k = 0; k < channel; k++) {
+		for (int x = 0; x < header.width; x++) {
+			for (int y = 0; y < header.height; y++) {
+				double xx = 1.0 * x * newWidth / header.width;
+				double yy = 1.0 * y * newHeight / header.height;
+				int srcX = static_cast<int>((xx + minX) * cosine + (yy + minY) * sine);
+				int srcY = static_cast<int>((yy + minY) * cosine - (xx + minX) * sine);
+				if (srcX >= 0 && srcX < header.width && srcY >= 0 && srcY < header.height) {
+					image.pixels[k][y][x] = pixels[k][srcY][srcX];	
+				}
+			}
+		}
+	}
+	return image;
+}
+
+/**
+* Constructs a scaled version of bmp::Image object
+* Input:
+*	xscale:	scale along width
+*	yscale:	scale along height
+*/
+bmp::Image bmp::Image::scale(double xscale, double yscale) {
+	bmp::Image image;
+	image.header = header;
+	int channel = header.bpp / 8;
+
+	// calculating new height, widht and size
+	image.header.width = static_cast<uint32_t>(ceil(xscale * header.width));
+	image.header.height = static_cast<uint32_t>(ceil(yscale * header.height));
+	image.header.imageSize = image.header.width * image.header.height * channel;
+	image.header.size += image.header.imageSize - header.imageSize;
+
+	image.pixels = new unsigned char** [channel];
+	for (int k = 0; k < channel; k++) {
+		image.pixels[k] = new unsigned char* [image.header.height];
+		for (int i = 0; i < image.header.height; i++) {
+			image.pixels[k][i] = new unsigned char[image.header.width];
+			for (int j = 0; j < image.header.width; j++) {
+				image.pixels[k][i][j] = 0;
+			}
+		}
+	}
+
+	// filling pixel values
+	for (int k = 0; k < channel; k++) {
+		for (int i = 0; i < header.height; i++) {
+			for (int j = 0; j < header.width; j++) {
+				int p = ceil(yscale * i);
+				int q = ceil(xscale * j);
+				image.pixels[k][p][q] = pixels[k][i][j];
+			}
+		}
+	}
+	return image;
+}
+
+/**
+* Constructs a new bmp::Image object by resetting one of the three channel
+* Input:
+*	chid:	'R' / 'G' / 'B'
+*/
+bmp::Image bmp::Image::resetChannel(char chid) {
+	bmp::Image image;
+	int channel = header.bpp / 8;
+	image.header = header;
+	int ch;
+	if (chid == 'R') ch = 0;
+	else if (chid == 'G') ch = 1;
+	else if (chid == 'B') ch = 2;
+	else throw std::runtime_error("Bad chid passed for resetChannel!");
+
+	image.pixels = new unsigned char** [channel];
+	for (int k = 0; k < channel; k++) {
+		image.pixels[k] = new unsigned char* [header.height];
+		for (int i = 0; i < header.height; i++) {
+			image.pixels[k][i] = new unsigned char[header.width];
+			for (int j = 0; j < header.width; j++) {
+				image.pixels[k][i][j] = (k != ch) ? pixels[k][i][j] : 0;
+			}
+		}
+	}
+	return image;
 }
 
 /**
