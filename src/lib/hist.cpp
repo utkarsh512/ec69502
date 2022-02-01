03ea57d4b6cab51e54d8606ad394f637f53c420a
Utkarsh Patel <imutkarshpatel@gmail.com>
2022-02-01 12:41:24 +0530

    performs histogram equalization and matching

--- src/lib/hist.cpp
+++ src/lib/hist.cpp
@@ -1,19 +1,4 @@
-
-// Module to perform histogram equalization and matching
-
-#include <iostream>
-#include <vector>
-#include <stdlib.h>
-#include <conio.h>
-#include <math.h>
-
-#include <opencv2/opencv.hpp>
-
-typedef std::vector< std::vector<double>> Vec;
-
-const int LEVEL = 256;
-const int INTENSITY_MAX = 255;
-const int INTENSITY_MIN = 0;
+#include "hist.h"
 
 /**
 * Function to generate histogram from the single-channel
@@ -214,7 +199,7 @@ void showHistogram(cv::Mat& image, int channel, const std::string& filename) {
 /**
 * Function to interactivily read an image using OpenCV
 */
-cv::Mat readImage(std::string& filename, const std::string& type, int FLAG=0) {
+cv::Mat readImage(std::string& filename, const std::string& type, int FLAG) {
 	std::cout << "Enter " << type << " " << " filename: ";
 	std::cin >> filename;
 	if (FLAG == 0) {
@@ -403,63 +388,4 @@ void MatchingManyToOne(cv::Mat& source, cv::Mat& target) {
 	showHistogram(outputImage, 2, "[2] Equalized histogram");
 
 	cv::waitKey();
-}
-
-int main() {
-	int option;
-	std::string filename1, filename2;
-
-	while (1) {
-		std::cout << "+++++++++++++++++++++++++++++++++++++++++\n";
-		std::cout << "Enter the opcode:\n";
-		std::cout << "\t1. Histogram equalization\n";
-		std::cout << "\t2. Histogram matching\n";
-		std::cout << "Select (1 or 2): ";
-		std::cin >> option;
-		std::cout << "+++++++++++++++++++++++++++++++++++++++++\n";
-		if (option == 1) {
-			// histogram equalization
-			std::cout << "Histogram equalization selected\n";
-			cv::Mat image = readImage(filename1, "input");
-			if (image.empty()) {
-				throw std::runtime_error("Error reading the file!");
-			}
-			if (image.channels() == 1) {
-				EqualizationSingle(image);
-			}
-			else if (image.channels() == 3) {
-				EqualizationMulti(image);
-			}
-			else {
-				throw std::runtime_error("Incompatible number of channels!");
-			}
-		}
-		else if (option == 2) {
-			// histogram matching selected
-			std::cout << "Histogram matching selected\n";
-			cv::Mat source = readImage(filename1, "source", 1);
-			cv::Mat target = readImage(filename2, "target", 2);
-			if (source.empty() || target.empty()) {
-				throw std::runtime_error("Error reading the files!");
-			}
-			int sourceChannels = source.channels();
-			int targetChannels = target.channels();
-			if (sourceChannels == 1 && targetChannels == 1) {
-				MatchingOneToOne(source, target);
-			}
-			else if (sourceChannels == 3 && targetChannels == 3) {
-				MatchingManyToMany(source, target);
-			}
-			else if (sourceChannels == 3 && targetChannels == 1) {
-				MatchingManyToOne(source, target);
-			}
-			else {
-				throw std::runtime_error("Incompatible number of channels in source and target files!");
-			}
-		}
-		else {
-			throw std::runtime_error("Wrong opcode received!");
-		}
-	}
-	return 0;
 }
