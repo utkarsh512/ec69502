e43618476de3a7eba555850e72d3bae5f3a7aae3
Utkarsh Patel <imutkarshpatel@gmail.com>
2022-02-15 20:40:40 +0530

    performs FFT and frequency domain filtering

---
+++ src/lib/freqfilt.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <math.h>
+#include <assert.h>
+#include <opencv2/opencv.hpp>
+
+using cd = std::complex<double>;
+
+const double PI = acos(-1);
+
+// FFT and IFFT
+
+void fft(std::vector<cd>&, bool);
+void fft(std::vector<std::vector<cd>>&, bool);
+void fftshift(std::vector<std::vector<cd>>&);
+
+// filters
+
+void ideal(std::vector<std::vector<cd>>&, double, int = 1, bool = false);
+void gaussian(std::vector<std::vector<cd>>&, double, int = 1, bool = false);
+void butterworth(std::vector<std::vector<cd>>&, double, int = 1, bool = false);
+
+// helper functions
+
+void transpose(std::vector<std::vector<cd>>&);
+void conjugate(std::vector<std::vector<cd>>&);
+void cofactor(std::vector<std::vector<cd>>&);
+std::vector<std::vector<cd>> readVector(const cv::Mat&);
+void writeVector(cv::Mat&, const std::vector<std::vector<cd>>&, bool = true);
+cd dot(const cd&, const cd&);
